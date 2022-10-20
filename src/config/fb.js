60a2c86101dcaace0074dd4e4db83ddde49b9dc7
sdarnadeem <sdarnadeem@gmail.com>
2022-10-20 15:55:05 +0530

    udpate firebase config

--- src/config/fb.js
+++ src/config/fb.js
@@ -5,16 +5,18 @@ import 'firebase/firestore';
 const firebaseConfig = {
   apiKey: 'AIzaSyCLA5A5hXhKBp7m1PC8I8D_LIu7ig9C-zg',
 
-  authDomain: 'webm.nasyxnadeem.dev',
+  authDomain: 'web-messenger-b5e45.firebaseapp.com',
+
+  databaseURL: 'https://web-messenger-b5e45-default-rtdb.firebaseio.com',
 
   projectId: 'web-messenger-b5e45',
-  databaseURL: 'web-messenger-b5e45.firebaseio.com',
 
   storageBucket: 'web-messenger-b5e45.appspot.com',
 
   messagingSenderId: '618759504810',
 
-  appId: '1:618759504810:web:d493f7908f53114a43e1d3',
+  appId: '1:618759504810:web:fb2ec7ee3eec4ab043e1d3',
+  measurementId: 'G-WCYNRETE13',
 };
 
 firebase.initializeApp(firebaseConfig);
